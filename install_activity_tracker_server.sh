413bd4f2ddc8616e8869f689542ae3fb567a9c13
David Milum <david.milum.89@gmail.com>
2015-11-25 23:19:11 -0500

    fix install script

--- install_activity_tracker_server.sh
+++ install_activity_tracker_server.sh
@@ -5,7 +5,7 @@ cd -
 
 echo sudo ln -s -T $GOPATH/bin/cmd /usr/bin/chromebus
 echo sudo ln -s -T $PWD/systemd/chromebus.service /usr/lib/systemd/system/chromebus.service
-sudo unlink /usr/bin/chromebus
-sudo unlink /usr/lib/systemd/system/chromebus.service
+[ -f /usr/bin/chromebus ] && sudo unlink /usr/bin/chromebus
+[ -f /usr/bin/chromebus ] && sudo unlink /usr/lib/systemd/system/chromebus.service
 sudo ln -s -T $GOPATH/bin/cmd /usr/bin/chromebus
 sudo ln -s -T $PWD/systemd/chromebus.service /usr/lib/systemd/system/chromebus.service
